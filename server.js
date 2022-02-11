3abe606bdf940521695f92c0736da37e34212a43
Clément Legros <71426481+ClementLegros@users.noreply.github.com>
2022-02-11 11:10:57 +0100

    fix route problem

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 app.use(express.static(path.join(__dirname, "/src")))
-app.use('/html', (req, res, next) => {
+
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'/src/index.html'))
 })
 app.listen(5005);
